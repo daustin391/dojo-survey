308a8be4722be6ecb23b65df2347d65b78ba75c3
Dave Austin <d.austin_391@yahoo.com>
2020-10-13 22:26:44 -0400

    final commit

--- form/urls.py
+++ form/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.form),
-]
+urlpatterns = [path("", views.form), path("result", views.result)]
--- form/views.py
+++ form/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def form(request):
-    context = {"": ""}
+    return render(request, "index.html")
 
-    return render(request, "index.html", context)
+
+def result(request):
+    if request.method == "POST":
+        context = {
+            "name": request.POST["name"],
+            "location": request.POST["location"],
+            "language": request.POST["language"],
+            "comment": request.POST["comment"],
+        }
+        return render(request, "result.html", context)
+    else:
+        return redirect("/")
